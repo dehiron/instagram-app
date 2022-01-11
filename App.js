8f7317b9a9b6990add66fc995c4a13764197267f
Hidehiro Aya <hi.dehiro424@gmail.com>
2022-01-11 18:32:29 +0900

    Implementing addNewPost component

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 // import { View, Text } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
+import NewPostScreen from './screens/NewPostScreen';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <HomeScreen />
+      {/* <HomeScreen /> */}
+      <NewPostScreen />
     </SafeAreaProvider>
   );
 }
---
+++ components/newPost/FormikPostUploader.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, Image, Button} from "react-native";
+import * as Yup from "yup";
+import { Formik } from "formik";
+import { Divider } from "react-native-elements";
+
+const PLACEHOLDER_IMG = "https://jmva.or.jp/wp-content/uploads/2018/07/noimage.png"
+
+const uploadPostSchema = Yup.object().shape({
+    imageUrl: Yup.string().url().required("A URL is required"),
+    caption: Yup.string().max(2200, "Caption has reached the character limit.")
+})
+
+
+const FormikPostUploader = () => {
+
+    const [thumbnailUrl, setThumbnailUrl] = useState(PLACEHOLDER_IMG)
+
+    return (
+        <Formik
+            initialValues={{caption: "", imageUrl: ""}}
+            onSubmit={(values) => console.log(values)}
+            validationSchema={uploadPostSchema}
+            validateOnMount={true} //Shareボタンのエラー対処
+        >
+        {({ handleBlur, handleChange, handleSubmit, values, errors, isValid }) => 
+            <>
+                <View style={{ 
+                    margin: 20,
+                    justifyContent: "space-between",
+                    flexDirection: "row"
+                }}
+                >
+                    <Image source = {{ uri : thumbnailUrl ? thumbnailUrl : PLACEHOLDER_IMG }} style={{width: 100, height: 100}}/>
+                    <View style={{flex: 1, marginLeft: 12}}>
+                        <TextInput 
+                            placeholder="Write a caption..." 
+                            placeholderTextColor="gray" 
+                            multiline={true}
+                            style={{color: "#fff", fontSize: 20}}
+                            onChangeText={handleChange("caption")}
+                            onBlur={handleBlur("caption")}
+                            value = {values.caption}
+                        />
+                    </View>
+                </View>
+                <Divider width={0.2} orientation="vertical" />
+                <TextInput
+                onChange={(e) => {setThumbnailUrl(e.nativeEvent.text)}}
+                    placeholder="Enter a image Url" 
+                    placeholderTextColor="gray" 
+                    style={{color: "#fff", fontSize: 18}}
+                    onChangeText={handleChange("imageUrl")}
+                    onBlur={handleBlur("imageUrl")}
+                    value = {values.imageUrl}
+                />
+                {errors.imageUrl && (
+                    <Text style={{ fontSize: 12, color: "red"}}>
+                        {errors.imageUrl}
+                    </Text>
+                )}
+
+                <Button onPress={handleSubmit} title="Share" disabled={!isValid} />
+
+            </>
+        }  
+
+
+
+        </Formik>
+
+        // <View>
+        //     <Text style={{ color: "white"}}>formik</Text>
+        // </View>
+    )
+}
+
+export default FormikPostUploader;
