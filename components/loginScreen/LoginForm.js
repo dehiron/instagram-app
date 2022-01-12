798c8fbe671204e075c019084c7162826cf4336f
Hidehiro Aya <hi.dehiro424@gmail.com>
2022-01-12 14:49:19 +0900

    Applying Formik features to LoginForm component

--- components/loginScreen/LoginForm.js
+++ components/loginScreen/LoginForm.js
@@ -1,46 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput, Pressable, TouchableOpacity } from "react-native";
+import { Formik } from "formik";
+import * as Yup from "yup"
+import Validator from "email-validator"
+
 
 const LoginForm = () => {
+
+    const LoginFormSchema = Yup.object().shape({
+        email: Yup.string().email().required("An email is required"),
+        password: Yup.string()
+            .required()
+            .min(6, "Your password has to have at least 8 characters")
+    })
+
     return(
         <View style={styles.wrapper}>
-            <View style={styles.inputField}>
-                <TextInput 
-                    placeholder = "Phone number, username or email"
-                    placeholderTextColor = "#444"
-                    autoCapitalize = "none"
-                    keyboardType = "email-address"
-                    textContentType = "emailAddress"
-                    autoFocus = {true}
-                />
-            </View>
+            <Formik
+                initialValues={{email: "", password:""}}
+                onSubmit={(values) => {
+                    console.log(values)
+                }}
+                validationSchema={LoginFormSchema}
+                validateOnMount={true}
+            >
+                {/* Formik使う時に必要なアノニマス関数で元々のView達をラップする（親<></>が必要） */}
+                {({handleChange, handleBlur, handleSubmit, values, isValid}) => (
+                    <>
+                        <View style={[
+                            styles.inputField,
+                            {
+                                borderColor: 
+                                    values.email.length < 1 || Validator.validate(values.email) 
+                                        ? "#ccc" 
+                                        : "red"
+                            }
+                        ]}
+                        >
+                            <TextInput 
+                                placeholder = "Phone number, username or email"
+                                placeholderTextColor = "#444"
+                                autoCapitalize = "none"
+                                keyboardType = "email-address"
+                                textContentType = "emailAddress"
+                                autoFocus = {true}
+                                onChangeText={handleChange("email")}
+                                onBlur={handleBlur("email")}
+                                value={values.email}
+                            />
+                        </View>
 
-            <View style={styles.inputField}>
-                <TextInput 
-                    placeholder = "Password"
-                    placeholderTextColor = "#444"
-                    autoCapitalize = "none"
-                    autoCorrect = {false}
-                    secureTextEntry = {true}
-                    textContentType = "password"
-                />
-            </View>
+                        <View style={[
+                            styles.inputField,
+                            {
+                                borderColor: 
+                                    values.password.length < 1 || values.password.length >= 6 
+                                        ? "#ccc" 
+                                        : "red"
+                            }
+                        ]}>
+                            <TextInput 
+                                placeholder = "Password"
+                                placeholderTextColor = "#444"
+                                autoCapitalize = "none"
+                                autoCorrect = {false}
+                                secureTextEntry = {true}
+                                textContentType = "password"
+                                onChangeText={handleChange("password")}
+                                onBlur={handleBlur("password")}
+                                value={values.password}
+                            />
+                        </View>
 
-            <View style={{ alignItems:"flex-end", marginBottom: 30 }}>
-                <Text style={{ color: "#6BB0F5" }}>Forgot password?</Text>
-            </View>
-            
-            <Pressable titleSize={20} style={styles.button} onPress={() => console.log(222)}>
-                <Text style={styles.buttonText}> Login</Text>
-            </Pressable>
+                        <View style={{ alignItems:"flex-end", marginBottom: 30 }}>
+                            <Text style={{ color: "#6BB0F5" }}>Forgot password?</Text>
+                        </View>
 
-            <View style={styles.signupContainer}>
-                <Text>Don't have an account?</Text>
-                <TouchableOpacity>
-                    <Text style={{ color: "#6BB0F5" }}> Sign Up</Text>
-                </TouchableOpacity>
-            </View>
-        
+                        <Pressable 
+                            titleSize={20} 
+                            style={[
+                                styles.button, 
+                                isValid ? null : styles.buttonNotValid 
+                                //ダイナミックスタイリング使いたいけどウェブでエラー起きるからこの対処
+                            ]} 
+                            onPress={handleSubmit}
+                            disabled={!isValid}
+                        >
+                            <Text style={styles.buttonText}> Login</Text>
+                        </Pressable>
+
+                        <View style={styles.signupContainer}>
+                            <Text>Don't have an account?</Text>
+                            <TouchableOpacity>
+                                <Text style={{ color: "#6BB0F5" }}> Sign Up</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                    </>
+                )}
+            </Formik>
         </View>
     )
 }
@@ -56,12 +114,16 @@ const styles = StyleSheet.create({
         marginBottom: 10,
         borderWidth: 1,
     },
+    //ダイナミックスタイリング使いたいけどウェブ上でエラー起きるから分ける
     button: {
         backgroundColor: "#0096F6",
         alignItems: "center",
         justifyContent: "center",
         minHeight: 42,
         borderRadius: 4,
+    },
+    buttonNotValid: {
+        backgroundColor: "#9ACAF7",
     },
     buttonText: {
         fontWeight: "600",
