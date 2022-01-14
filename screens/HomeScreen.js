5ad598ee866ad0f9fa127d3e8b672bef6cb7a64e
Hidehiro Aya <hi.dehiro424@gmail.com>
2022-01-14 16:21:19 +0900

    Test using onSnapshot(collectionGroup(..)...) code to fetch posts data

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import { collectionGroup, onSnapshot } from 'firebase/firestore';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import BottomTabs, { bottomTabIcons } from '../components/home/BottomTabs';
@@ -6,8 +7,20 @@ import Header from '../components/home/Header';
 import Post from '../components/home/Post';
 import Stories from '../components/home/Stories';
 import { POSTS } from '../data/posts';
+import { db } from '../firebase';
 
 const HomeScreen = (props) => {
+
+    useEffect(() => {
+
+        onSnapshot(
+            collectionGroup(db, "posts"), 
+            (snapshot) => {
+                console.log(snapshot.docs.map(doc => doc.data()))  
+            })
+                        
+    },[])
+
     return(
         <SafeAreaView style = {styles.container} >
             <Header navigation = {props.navigation} />
