8c33ccea3d94babba2cba281fb45bf4a40ae50e1
Hidehiro Aya <hi.dehiro424@gmail.com>
2022-01-15 22:06:10 +0900

    Add scroll view on Login and Signup screens

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,18 +1,19 @@
 import React from "react";
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View, Image, ScrollView } from "react-native";
 import SignupForm from "../components/signupScreen/SignupForm";
 
 const INSTAGRAM_LOGO = "https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Instagram-256.png"
 
 const SignupScreen = (props) => {
     return (
-        <View style={styles.container}>
-            <View style={styles.logoContainer}>
-                <Image source={{uri : INSTAGRAM_LOGO, height: 100, width: 100}} />
+            <View style={styles.container}>
+                <ScrollView>
+                    <View style={styles.logoContainer}>
+                        <Image source={{uri : INSTAGRAM_LOGO, height: 100, width: 100}} />
+                    </View>
+                    <SignupForm navigation={props.navigation}/>
+                </ScrollView>
             </View>
-            <SignupForm navigation={props.navigation}/>
-        </View>
-
     )
 }
 
